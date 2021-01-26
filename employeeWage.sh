cdaee3dced24a3479ddd4d817cbffa6437411a49
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-01-26 21:16:14 +0530

    ADD.Calculate Daily Employee Wage

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -x
 isPresent=1;
-randomCheck=$((RANDOM %3));
+empRatePerHr=20;
+randomCheck=$((RANDOM %2));
 if [ $isPresent -eq $randomCheck ]; then
         echo "Employee present";
+        empHrs=8;
+        wage=$(($empRatePerHr*$empHrs));
 else
         echo "Employee absent";
+        wage=0;
 fi
-
+echo "Salary=$wage";
