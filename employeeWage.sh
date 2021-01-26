b34d39bd0812456a8e509f0104d50cdac8b2b0a2
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-01-26 21:19:49 +0530

    ADD.added Part time Employee and Wage

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,9 +1,18 @@
 #!/bin/bash -x
-isPresent=1;
-randomCheck=$((RANDOM %2));
+isPresent=2;
+empRatePerHr=20;
+isPartTime=1;
+randomCheck=$((RANDOM %3));
 if [ $isPresent -eq $randomCheck ]; then
         echo "Employee present";
+        empHrs=8;
+        wage=$(($empRatePerHr*$empHrs));
+elif [ $isPartTime -eq $randomCheck ]; then
+        echo "Employee Part Time";
+        empHrs=4;
+        wage=$(($empRatePerHr*$empHrs));
 else
         echo "Employee absent";
+        wage=0;
 fi
-
+echo "Salary=$wage";
