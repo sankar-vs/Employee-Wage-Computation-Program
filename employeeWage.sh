05016b19b3dc3ce0d8b34ab6aa8d601369ad8f52
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-01-26 21:26:50 +0530

    ADD.Calculating Wages for a Month

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,14 +1,20 @@
 #!/bin/bash -x
-isFullTime=1;
-empRatePerHr=20;
-isPartTime=2;
-empCheck=$((RANDOM %3));
-case $empCheck in
-        $isFullTime)
-                empHrs=8;;
-        $isPartTime)
-                empHrs=4;;
-        *)
-                empHrs=0;;
-esac
-Salary=$(($empRatePerHr*$empHrs));
+isFullTime=1
+empRatePerHr=20
+isPartTime=2
+workingDays=20
+for (( day=1 ; day<=$workingDays ; day++))
+do
+        empCheck=$((RANDOM %3))
+        case $empCheck in
+                $isFullTime)
+                        empHrs=8;;
+                $isPartTime)
+                        empHrs=4;;
+                *)
+                        empHrs=0;;
+        esac
+salary=$(($empRatePerHr*$empHrs))
+totalSalary=$(($totalSalary+$salary))
+done
+echo $totalSalary
